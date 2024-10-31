efb951ad06cd8427db4c4c4c943a765b18919bcc
Agustin Gomez <agustingomezdev@gmail.com>
2024-10-31 17:44:24 +0100

    add header, hero & projectCard

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary: "#4283c1",
+				secondary: "#2a5176",
+				lightAccent: "#93b6d6",
+				darkAccent: "#1c242c",
+				bgColor: "#010911",
+				textColor: "#e2f0fd"
+			}
+		},
 	},
 	plugins: [],
 }
